8004961d5871401145955d942728be8985ea92df
muizu <muizu0920555@protonmail.com>
2023-06-26 14:29:47 +0900

    finish

--- main.go
+++ main.go
@@ -26,9 +26,43 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) { //c=gin.Context
+	var newAlbum album //type album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil { //ここでreq.bodyを解析して、そのオブジェクトをnewAlbumのポインタに紐づけている
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)            //ここでスライスにデータをぶち込んでいる
+	c.IndentedJSON(http.StatusCreated, newAlbum) //ここで新しくデータが追加された、newAlbumをクライアント側に返している
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id") //ここでurlからreq.params.idを取ってきている
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a) //jsonにシリアライズしている(json型にしているということ)
+			return                           //関数毎終わらせている
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
